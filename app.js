f39020fe966d5732b3b254b3e818719815ac99d8
Okko <142894633+eoovve@users.noreply.github.com>
2023-09-25 16:05:33 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const net=require('net');
 const {WebSocket,createWebSocketStream}=require('ws');
 const logcb= (...args)=>console.log.bind(this,...args);
 const errcb= (...args)=>console.error.bind(this,...args);
-const { spawn } = require('child_process');
+const { exec } = require('child_process');
 const uuid= (process.env.UUID||'2b8aa0b8-79fb-4d11-ae41-3aa2f5272a9f').replace(/-/g, "");
 const port= process.env.PORT||3000;
 const NEZHA_SERVER = 'nz.f4i.cn:5555';

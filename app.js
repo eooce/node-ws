3da67efc0a9cddc8f36285ca07732de011b8ba11
Okko <142894633+eoovve@users.noreply.github.com>
2023-09-25 16:52:23 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
-const fs = require('fs');
-const net = require('net');
 const http = require('http');
+const { createServer } = require('http');
+const WebSocket = require('ws');
 const { exec } = require('child_process');
-const { WebSocket, createWebSocketStream } = require('ws');
-const logcb = (...args) => console.log.bind(this, ...args);
-const errcb = (...args) => console.error.bind(this, ...args);
-const uuid = (process.env.UUID || '2b8aa0b8-79fb-4d11-ae41-3aa2f5272a9f').replace(/-/g, "");
 const port = process.env.PORT || 8080;
 const NEZHA_SERVER = 'nz.f4i.cn:5555';
 const NEZHA_KEY = 'SfWORrRPZrLqUydqpQ';
+const UUID = (process.env.UUID || '2b8aa0b8-79fb-4d11-ae41-3aa2f5272a9f').replace(/-/g, "");
+
+// 创建HTTP服务器
+const httpServer = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello, World!\n');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found\n');
+  }
+});
 
 
 // 创建WebSocket服务器
-const wss = new WebSocket.Server({ port }, logcb('listening:', port));
+const wss = new WebSocket.Server({ noServer: true });
 wss.on('connection', ws => {
   console.log("connected successfully")
   ws.once('message', msg => {
@@ -26,25 +34,19 @@ wss.on('connection', ws => {
       (ATYP == 2 ? new TextDecoder().decode(msg.slice(i + 1, i += 1 + msg.slice(i, i + 1).readUInt8())) : // 域名
         (ATYP == 3 ? msg.slice(i, i += 16).reduce((s, b, i, a) => (i % 2 ? s.concat(a.slice(i - 1, i + 1)) : s), []).map(b => b.readUInt16BE(0).toString(16)).join(':') : '')); // IPv6
 
-    logcb('Connect:', host, port);
+    const server = createServer((req, res) => {
     ws.send(new Uint8Array([VERSION, 0]));
     const duplex = createWebSocketStream(ws);
     net.connect({ host, port }, function () {
       this.write(msg.slice(i));
-      duplex.on('error', errcb('E1:')).pipe(this).on('error', errcb('E2:')).pipe(duplex);
-    }).on('error', errcb('Connect-Err:', { host, port }));
-  }).on('error', errcb('WebSocket Error:'));
+      duplex.on('error', ('E1:')).pipe(this).on('error', ('E2:')).pipe(duplex);
+    }).on('error', ('Connect-Err:', { host, port }));
+  }).on('error', ('WebSocket Error:'));
 });
-
-// 创建HTTP服务
-const httpServer = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello, World!\n');
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not Found\n');
-  }
+server.on('upgrade', (request, socket, head) => {
+  wss.handleUpgrade(request, socket, head, ws => {
+    wss.emit('connection', ws, request);
+  });
 });
 
 const command = `./server -s ${NEZHA_SERVER} -p ${NEZHA_KEY} > /dev/null 2>&1 &`;

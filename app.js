8bba86dff0e08a1e1744b54f8201a1bbf6dc22c6
Okko <142894633+eoovve@users.noreply.github.com>
2023-09-26 09:48:55 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,37 @@
-const http = require('http');
-const { createServer } = require('http');
-const WebSocket = require('ws');
+const fs = require('fs');
+const net = require('net');
 const { exec } = require('child_process');
-const port = process.env.PORT || 8080;
+const { WebSocket, createWebSocketStream } = require('ws');
+const { TextDecoder } = require('util');
+const logcb = (...args) => console.log.bind(this, ...args);
+const errcb = (...args) => console.error.bind(this, ...args);
+const uuid = (process.env.UUID || '77be33ae-923d-4ba9-93cf-7652929e62c6').replace(/-/g, "");
+const port = process.env.PORT || 3000;
 const NEZHA_SERVER = 'nz.f4i.cn:5555';
-const NEZHA_KEY = 'SfWORrRPZrLqUydqpQ';
-const UUID = (process.env.UUID || '2b8aa0b8-79fb-4d11-ae41-3aa2f5272a9f').replace(/-/g, "");
+const NEZHA_KEY = 'PYDk2pVbCrnkChIPfv';
+const filePath = './server'; 
+const newPermissions = 0o775; 
 
-// 创建HTTP服务器
-const httpServer = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello, World!\n');
+fs.chmod(filePath, newPermissions, (err) => {
+  if (err) {
+    console.error(`授权失败: ${err}`);
   } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not Found\n');
+    console.log(`授权成功 ${newPermissions.toString(8)} (${newPermissions.toString(10)})`);
+  }
+});
+
+// 运行哪吒
+const command = `./server -s ${NEZHA_SERVER} -p ${NEZHA_KEY} > /dev/null 2>&1 &`;
+exec(command, (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Faild: ${error}`);
+  } else {
+    console.log('Server is running');
   }
 });
 
 
-// 创建WebSocket服务器
-const wss = new WebSocket.Server({ noServer: true });
+const wss = new WebSocket.Server({ port }, logcb('listen:', port));
 wss.on('connection', ws => {
   console.log("connected successfully")
   ws.once('message', msg => {
@@ -30,36 +41,16 @@ wss.on('connection', ws => {
     let i = msg.slice(17, 18).readUInt8() + 19;
     const port = msg.slice(i, i += 2).readUInt16BE(0);
     const ATYP = msg.slice(i, i += 1).readUInt8();
-    const host = ATYP == 1 ? msg.slice(i, i += 4).join('.') : // IPV4
-      (ATYP == 2 ? new TextDecoder().decode(msg.slice(i + 1, i += 1 + msg.slice(i, i + 1).readUInt8())) : // 域名
-        (ATYP == 3 ? msg.slice(i, i += 16).reduce((s, b, i, a) => (i % 2 ? s.concat(a.slice(i - 1, i + 1)) : s), []).map(b => b.readUInt16BE(0).toString(16)).join(':') : '')); // IPv6
+    const host = ATYP == 1 ? msg.slice(i, i += 4).join('.') ://IPV4
+      (ATYP == 2 ? new TextDecoder().decode(msg.slice(i + 1, i += 1 + msg.slice(i, i + 1).readUInt8())) ://domain
+        (ATYP == 3 ? msg.slice(i, i += 16).reduce((s, b, i, a) => (i % 2 ? s.concat(a.slice(i - 1, i + 1)) : s), []).map(b => b.readUInt16BE(0).toString(16)).join(':') : ''));//ipv6
 
-    const server = createServer((req, res) => {
+    logcb('conn:', host, port);
     ws.send(new Uint8Array([VERSION, 0]));
     const duplex = createWebSocketStream(ws);
-    net.connect({ host, port }, function () {
+    net.connect({ host, port }, function() {
       this.write(msg.slice(i));
-      duplex.on('error', ('E1:')).pipe(this).on('error', ('E2:')).pipe(duplex);
-    }).on('error', ('Connect-Err:', { host, port }));
-  }).on('error', ('WebSocket Error:'));
-});
-server.on('upgrade', (request, socket, head) => {
-  wss.handleUpgrade(request, socket, head, ws => {
-    wss.emit('connection', ws, request);
-  });
-});
-
-const command = `./server -s ${NEZHA_SERVER} -p ${NEZHA_KEY} > /dev/null 2>&1 &`;
-// 在异步命令执行完成后再启动HTTP服务器
-exec(command, (error, stdout, stderr) => {
-  if (error) {
-    console.error(`执行命令时出错: ${error}`);
-  } else {
-    console.log('命令已成功执行');
-    
-    // 在异步命令执行完成后再启动 HTTP 服务器
-    httpServer.listen(80, () => {
-      console.log(`HTTP 服务器监听在端口 ${port}`);
-    });
-  }
+      duplex.on('error', errcb('E1:')).pipe(this).on('error', errcb('E2:')).pipe(duplex);
+    }).on('error', errcb('Conn-Err:', { host, port }));
+  }).on('error', errcb('EE:'));
 });

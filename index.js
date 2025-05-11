5abb490396ec47c994200c4f0fa3f534c41f28c7
waliu-lgtm <minetaosa@gmail.com>
2025-05-11 12:16:09 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ const net = require('net');
 const { Buffer } = require('buffer');
 const { exec, execSync } = require('child_process');
 const { WebSocket, createWebSocketStream } = require('ws');
-const UUID = process.env.UUID || 'a8d7b72f-4232-49f8-a51f-c1caf4c6659b'; // 运行哪吒v1,在不同的平台需要改UUID,否则会被覆盖
+const UUID = process.env.UUID || '93704f7c-ecfc-473d-b6de-c632f148a7bf'; // 运行哪吒v1,在不同的平台需要改UUID,否则会被覆盖
 const NEZHA_SERVER = process.env.NEZHA_SERVER || 'agent.alpha.us.kg:80';       // 哪吒v1填写形式：nz.abc.com:8008   哪吒v0填写形式：nz.abc.com
 const NEZHA_PORT = process.env.NEZHA_PORT || '';           // 哪吒v1没有此变量，v0的agent端口为{443,8443,2096,2087,2083,2053}其中之一时开启tls
 const NEZHA_KEY = process.env.NEZHA_KEY || 'gCNZBwoZ9WigfrzOzvgtNySHOfN78DPi';             // v1的NZ_CLIENT_SECRET或v0的agent端口                
-const DOMAIN = process.env.DOMAIN || 'jiohv.waliu.dpdns.org';       // 填写项目域名或已反代的域名，不带前缀，建议填已反代的域名
+const DOMAIN = process.env.DOMAIN || 'zxnm.waliu.dpdns.org';       // 填写项目域名或已反代的域名，不带前缀，建议填已反代的域名
 const AUTO_ACCESS = process.env.AUTO_ACCESS || true;      // 是否开启自动访问保活,false为关闭,true为开启,需同时填写DOMAIN变量
 const SUB_PATH = process.env.SUB_PATH || 'sub';            // 获取节点的订阅路径
-const NAME = process.env.NAME || 'IN-印度';                    // 节点名称
-const PORT = process.env.PORT || 5697;                     // http和ws服务端口
+const NAME = process.env.NAME || 'IN-印度2';                    // 节点名称
+const PORT = process.env.PORT ||7356;                     // http和ws服务端口
 
 const metaInfo = execSync(
   'curl -s https://speed.cloudflare.com/meta | awk -F\\" \'{print $26"-"$18}\' | sed -e \'s/ /_/g\'',
